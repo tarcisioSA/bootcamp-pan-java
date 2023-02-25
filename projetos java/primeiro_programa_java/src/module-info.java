6d387a1d60e2033bb085d89927bc7809118ad8ef
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-02-24 23:26:50 -0300

    primeiro programa

---
+++ projetos java/primeiro_programa_java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Almei
+ *
+ */
+module primeiro_programa_java {
+}
---
+++ projetos java/primeiro_programa_java/src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package primeiro_programa_java;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		 System.out.println("A soma é " + (a + b));
+
+	}
+
+}
