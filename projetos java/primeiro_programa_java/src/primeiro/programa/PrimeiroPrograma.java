50d53283e7ea320a7e0a993db797dc5ccbc4f62e
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-02-24 23:55:50 -0300

    att

---
+++ projetos java/primeiro_programa_java/src/primeiro/programa/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package primeiro.programa;
+
+import primeiro.programa.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	Gato gato = new Gato();
+	
+	Livros livros = new Livros();
+	System.out.println(gato);
+
+	/*public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		 System.out.println("A soma é " + (a + b));
+
+	}*/
+
+}
+
+class Livros{
+	private String nome;
+	private String npag;
+
+}
