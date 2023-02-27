85a44e79387ef9053de1b90cb0f9dd2467a0f62e
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-02-27 15:42:13 -0300

    comit

--- projetos java/bootcamp_pan_java/src/bootcamp/java/TiposVariaveis.java
+++ projetos java/bootcamp_pan_java/src/bootcamp/java/TiposVariaveis.java
@@ -1,6 +1,18 @@
 package bootcamp.java;
 public class TiposVariaveis {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        double salarioMinimo = 2500;
+        int numero;
+        numero = 15;
+
+        System.out.println("O número é: " + numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+        
+
     }
 }
---
+++ projetos java/bootcamp_pan_java/src/bootcamp/java/operadores.java
@@ -0,0 +1,17 @@
+package bootcamp.java;
+
+public class operadores {
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+    
+}
