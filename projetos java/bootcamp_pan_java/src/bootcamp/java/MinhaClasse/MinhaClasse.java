3a4b15abb6fca7f80a1dfb565c51dbe560f8d0a0
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-02-27 14:30:58 -0300

    organizaçao

---
+++ projetos java/bootcamp_pan_java/src/bootcamp/java/MinhaClasse/MinhaClasse.java
@@ -0,0 +1,20 @@
+package bootcamp.java.MinhaClasse;
+
+public class MinhaClasse{
+
+    public static void main(String [] args ) { 
+        String primeiroNome = "Tarcisio";
+        String segundoNome = "Sousa Almeida";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+       
+        System.out.println(nomeCompleto);
+        
+    }
+
+        public static String nomeCompleto (String primeiroNome, String segundoNome){
+            return primeiroNome.concat(" ").concat(segundoNome);
+
+        }
+
+}
