a4e2c093427f4baaf70652046f1c44775395d783
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2023-02-27 19:54:31 -0300

    smartTv

---
+++ projetos java/bootcamp_pan_java/src/bootcamp/java/UsuarioTv.java
@@ -0,0 +1,30 @@
+package bootcamp.java;
+
+public class UsuarioTv {
+    public static void main(String [] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Novo canal: " + smartTv.canal);
+        
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Tv Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status > TVligada?: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Ligada? " + smartTv.ligada);
+        
+    }
+}
